67558e9b49fad11dccf252a7717fc3986f8fddc9
lakshyabaresa <lakshyabaresa2005@gmail.com>
2026-06-11 19:25:56 +0530

    Create Printing entered input.cpp

---
+++ Printing entered input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string input;
+
+    cout << "Enter something: ";
+    getline(cin, input);
+
+    cout << "You entered: " << input << endl;
+
+    return 0;
+}
